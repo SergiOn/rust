647a4dd998571ec30212a8317b658da62b6d7ce5
SergiOn <onishhenko@gmail.com>
2021-06-21 00:12:15 +0300

    #rust-fundamentals # Section 3 Building a Command Line Application # 11. Finishing Touches

--- Courses/Learn-Rust-by-Building-Real-Applications/mars_calc/src/main.rs
+++ Courses/Learn-Rust-by-Building-Real-Applications/mars_calc/src/main.rs
@@ -6,20 +6,22 @@ fn main() {
     // println!("{}", a);
     // println!("{}", b);
 
+    println!("Enter your weight (kg): ");
     let mut input = String::new();
     // let mut s = input;
 
-    let s1 = &input;
-    let s2 = &input;
-    println!("{} {}", s1, s2);
+    // let s1 = &input;
+    // let s2 = &input;
+    // println!("{} {}", s1, s2);
 
-    some_fn(&mut input);
-    io::stdin().read_line(&mut input);
+    // some_fn(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
+    let weight: f32 = input.trim().parse().unwrap();
 
     println!("Hello, world!");
     println!("input: {}", input);
 
-    let mars_weight = calculate_weight_on_mars(100.0);
+    let mars_weight = calculate_weight_on_mars(weight);
     println!("Weight on Mars: {}kg", mars_weight);
 }
 
