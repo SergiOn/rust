1dbd8a42fc7a0f3cf75f42ad340301fc828ec808
SergiOn <onishhenko@gmail.com>
2021-06-20 22:34:47 +0300

    #rust-fundamentals # Section 3 Building a Command Line Application # 5. Macros

--- Courses/Learn-Rust-by-Building-Real-Applications/mars_calc/src/main.rs
+++ Courses/Learn-Rust-by-Building-Real-Applications/mars_calc/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     println!("Hello, world!");
 
-    println!("{}", calculate_weight_on_mars(100.0));
+    let mars_weight = calculate_weight_on_mars(100.0);
+    println!("Weight on Mars: {}kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
-    weight
+    weight / 9.81 * 3.711
 }
