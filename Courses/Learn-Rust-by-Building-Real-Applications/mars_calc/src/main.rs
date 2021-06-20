b364ea0a0d5612bd3417b5e5cd27ca9dea12b686
SergiOn <onishhenko@gmail.com>
2021-06-20 22:42:46 +0300

    #rust-fundamentals # Section 3 Building a Command Line Application # 7. The Standard Library

--- Courses/Learn-Rust-by-Building-Real-Applications/mars_calc/src/main.rs
+++ Courses/Learn-Rust-by-Building-Real-Applications/mars_calc/src/main.rs
@@ -1,4 +1,9 @@
+use std::io;
+
 fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+
     println!("Hello, world!");
 
     let mars_weight = calculate_weight_on_mars(100.0);
