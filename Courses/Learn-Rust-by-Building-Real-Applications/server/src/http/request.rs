647995988ffd88cc331f217efe52a6622b7a9820
SergiOn <onishhenko@gmail.com>
2021-06-22 18:48:20 +0300

    #rust-fundamentals # Section 4 Building a HTTP Server From Scratch # 18. Advanced Error Handling

--- Courses/Learn-Rust-by-Building-Real-Applications/server/src/http/request.rs
+++ Courses/Learn-Rust-by-Building-Real-Applications/server/src/http/request.rs
@@ -2,6 +2,8 @@ use crate::http::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -13,8 +15,14 @@ impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
     // GET /search?name=abc&sort=1 HTTP/1.1\r\n...HEADERS...
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        todo!()
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        // match str::from_utf8(buf) {
+        //     Ok(request) => {}
+        //     Err(_) => return Err(ParseError::InvalidEncoding)
+        // }
+
+        // let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+        let request = str::from_utf8(buf)?;
     }
 }
 
@@ -36,6 +44,12 @@ impl ParseError {
     }
 }
 
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
 impl Display for ParseError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         write!(f, "{}", self.message())
@@ -48,6 +62,4 @@ impl Debug for ParseError {
     }
 }
 
-impl Error for ParseError {
-
-}
+impl Error for ParseError {}
