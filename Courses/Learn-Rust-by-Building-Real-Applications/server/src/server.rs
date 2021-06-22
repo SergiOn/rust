17e1fa5ede0ce3571a3acdb77525021ee7a07218
SergiOn <onishhenko@gmail.com>
2021-06-22 17:47:11 +0300

    #rust-fundamentals # Section 4 Building a HTTP Server From Scratch # 16. Traits and Type Conversions

--- Courses/Learn-Rust-by-Building-Real-Applications/server/src/server.rs
+++ Courses/Learn-Rust-by-Building-Real-Applications/server/src/server.rs
@@ -1,6 +1,8 @@
 use std::net::{TcpListener, SocketAddr, TcpStream};
+use std::convert::{TryFrom, TryInto};
 use std::io::Read;
 use std::io::Error;
+use crate::http::Request;
 
 pub struct Server {
     addr: String,
@@ -32,6 +34,15 @@ impl Server {
                     match tcpStream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            // Request::try_from(&buffer as &[u8]);
+                            // Request::try_from(&buffer[..]);
+                            // let result: &Result<Request, _> = &buffer.try_into();
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse a request: {}", e)
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
