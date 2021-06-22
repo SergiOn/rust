c0023a8a3c06b473a6392adb6ac8e9249515fab2
SergiOn <onishhenko@gmail.com>
2021-06-22 18:00:10 +0300

    #rust-fundamentals # Section 4 Building a HTTP Server From Scratch # 17. Custom Errors

--- Courses/Learn-Rust-by-Building-Real-Applications/server/src/http/request.rs
+++ Courses/Learn-Rust-by-Building-Real-Applications/server/src/http/request.rs
@@ -1,5 +1,7 @@
 use crate::http::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
 
 pub struct Request {
     path: String,
@@ -8,9 +10,44 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n...HEADERS...
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         todo!()
     }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {
+
 }
